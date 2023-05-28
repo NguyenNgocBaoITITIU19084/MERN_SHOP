88450b7da991156f4e845d9dce50825e31fdb632
NguyenNgocBao <ititiu19084@student.hcmiu.edu.vn>
2023-05-28 11:23:20 +0700

    Sign-up Page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LoginPage } from "./Routers";
+import { LoginPage, SignupPage } from "./Routers";
 import "./App.css";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/sign-up" element={<SignupPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
