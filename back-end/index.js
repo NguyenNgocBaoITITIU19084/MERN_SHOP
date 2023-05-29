18df68a5f7948e2ab3017c1a8b3af15dd0a6e356
NguyenNgocBao <ititiu19084@student.hcmiu.edu.vn>
2023-05-29 14:04:06 +0700

    user model + sendEmail + jwtToken + multer

--- back-end/index.js
+++ back-end/index.js
@@ -3,10 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const fileUpload = require("express-fileupload");
 
 const MongoDB = require("./db/MongoDB");
 const ErrorHandler = require("./utils/ErrorHandler");
+
+// Import Router
+const user = require("./controller/user");
 
 const PORT = process.env.PORT;
 
@@ -15,8 +17,10 @@ MongoDB.connection();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/", express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-app.use(fileUpload({ useTempFiles: true }));
+
+app.use("/api/v2/user", user);
 
 app.use(ErrorHandler);
 
